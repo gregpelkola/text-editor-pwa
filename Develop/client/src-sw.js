eec7535eb7f7ab9095b463c11e90c9b070f40c05
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-01-24 21:36:29 -0600

    added registerRoute function for cache

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,17 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  /\.(css|js|png|jpg|jpeg|svg|gif)$/,
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache assets for 7 days
+      }),
+    ],
+  })
+);
